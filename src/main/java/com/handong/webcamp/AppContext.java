67febe98f3049eb9bce567beb6deeb3e67f0226f
wonsik <cws0173@gmail.com>
2020-01-15 17:35:24 +0900

    add app context

---
+++ src/main/java/com/handong/webcamp/AppContext.java
@@ -0,0 +1,45 @@
+package com.handong.webcamp;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.mysql.jdbc.Driver;
+
+@Configuration
+@PropertySource("/com/handong/webcamp/database.properties")
+public class AppContext {
+	@Value("${db.driverClass}") Class<? extends Driver> driverClass;
+	@Value("${db.url}") String url;
+	@Value("${db.username}") String username;
+	@Value("${db.password}") String password;
+	
+	/*
+	 * DB Connection and Transaction
+	 * 
+	 */
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass(this.driverClass);			
+		dataSource.setUrl(this.url);
+		dataSource.setUsername(this.username);
+		dataSource.setPassword(this.password);
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		
+		return tm;
+	}
+}
